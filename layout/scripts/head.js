5b7dab17e574ddd5ed49aba14cc0e80dee939743
bwc1024 <126658633+bwc1024@users.noreply.github.com>
2023-03-14 14:42:48 +0800

    422
    54

--- layout/scripts/head.js
+++ layout/scripts/head.js
@@ -1,15 +1,17 @@
-header='<header id="header" class="hoc clear">\
+header='<div class="wrapper row1 menu">\
+        <header id="header" class="hoc clear">\
         <div id="logo" class="fl_left"> \
             <h1 class="logoname"><a href="index.html">語言<span>科</span>學</a></h1>\
         </div>\
         <nav id="mainav" class="fl_right">\
             <ul class="clear">\
-                <li class="active"><a href="index.html">全部課程</a></li>\
-                <li><a class="active" href="#">關於我們</a></li>\
-                <li><a class="active" href="#">相關連結</a></li>\
-                <li><a href="#">歡迎捐款</a></li>\
+                <li><a class="active navli" href="#">全部課程</a></li>\
+                <li><a class="active navli" href="#">關於我們</a></li>\
+                <li><a class="active navli" href="#">相關連結</a></li>\
+                <li><a class="active navli" href="#">歡迎捐款</a></li>\
             </ul>\
         <form action="#"><select><option selected="selected" value="">MENU</option><option value="index.html">全部課程</option><option value="#">關於我們</option><option value="#">相關連結</option><option value="#">歡迎捐款</option></select></form></nav>\
-    </header>'
+        </header>\
+        </div>'
 
 document.write(header);
